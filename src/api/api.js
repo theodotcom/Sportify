c02128b2197ef0c2e9f7248dcf840eb7f8ce8373
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-12-04 15:36:28 +0100

    Error 404 add

--- src/api/api.js
+++ src/api/api.js
@@ -3,21 +3,33 @@ class Api {
     getPerformance(userId){
         return fetch(`http://localhost:3000/user/${userId}/performance`)
             .then(data => data.json())
+            .catch((error) => {
+                console.error('Error:', error);
+              });
     }
 
     getActivity(userId){
         return fetch(`http://localhost:3000/user/${userId}/activity`)
             .then(data => data.json())
+            .catch((error) => {
+                console.error('Error:', error);
+              });
     }
 
     getAverageSessions(userId){
         return fetch(`http://localhost:3000/user/${userId}/average-sessions`)
             .then(data => data.json())
+            .catch((error) => {
+                console.error('Error:', error);
+              });
     }
 
     getUser(userId){
         return fetch(`http://localhost:3000/user/${userId}`)
             .then(data => data.json())
+            .catch((error) => {
+                console.error('Error:', error);
+              });
     }
 
 }
